4bad6218aab4d047e75e52bade5239303228aa8d
aemullin <amullin68@gmail.com>
2022-02-08 19:53:37 -0800

    update movie-card proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './movie-card.scsss'
 
 export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
+
         return <div className="movie-card" onClick={() => {onMovieClick(movie);}}>{movie.Title}</div>;
     }
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
