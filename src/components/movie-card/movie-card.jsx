b69c89ccbf054981aa860f18ac31ab2d51bbc618
aemullin <amullin68@gmail.com>
2022-02-08 19:59:12 -0800

    fix
    removed extra s

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './movie-card.scsss'
+import './movie-card.scss'
 
 export class MovieCard extends React.Component {
     render() {
