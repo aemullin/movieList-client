8eb81a2bc2c526d73289038844a9b8b3836fd6e6
aemullin <amullin68@gmail.com>
2022-02-21 10:50:25 -0800

    Update movie card with add and remove to favorites button

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,23 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/card';
+
+import { Link } from 'react-router-dom';
 
 import './movie-card.scss'
 
 export class MovieCard extends React.Component {
+    onAddFavorite = (e,movie) => {
+        e.preventDefault();
+        const Username = localStorage.getItem('user');
+        const token = localStorage.getItem('token');
+
+        axios.post(`https://movie-list-api-5858.herokuapp.com/users/${Username}/favorites/${this.props.movie._id}`, {
+            headers: {Authorization: `Bearer ${token}` }}
+        )
+        .then((response) => {
+            console.log(response);
+            alert(`${movie.Title} added to favorites`);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    onRemoveFavorite = (e,movies) => {
+        e.preventDefault();
+        const Username = localStorage.getItem('user');
+        const token = localStorage.getItem('token');
+
+        axios.delete(`https://movie-list-api-5858.herokuapp.com/users/${Username}/favorites/${this.props.movie._id}`, {
+            headers: {Authorization: `Bearer ${token}` }}
+        )
+        .then((response) => {
+            console.log(response);
+            alert(`${movie.Title} removed from favorites`);
+            this.componentDidMount();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     render() {
-        const { movie, onMovieClick } = this.props;
+        const { movie } = this.props;
 
         return (
-            <Card className="text-center">
+            <Card className="text-center" height="600px">
                 <div className="card-poster text-center">
                     <Card.Img variant="top" src={movie.ImagePath} crossOrigin="true"/>
                 </div>
                 <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onMovieClick(movie)} variant="primary">Open</Button>
+                    <Card.Title>{movie.Title}</Card.Title> 
+                    <Link to={`/movies/${movie._id}`}>
+                        <Button variant="primary">Open</Button>
+                    </Link>
+                    <Button variant="secondary" onClick={this.onAddFavorite}>+ Favorites</Button>
+                    <Button variant="danger" onClick={this.onRemoveFavorite}> - Favorites </Button>
                 </Card.Body>
             </Card>
         );
@@ -27,6 +68,5 @@ export class MovieCard extends React.Component {
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    })
 };
