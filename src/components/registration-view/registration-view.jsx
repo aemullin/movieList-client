0e6721dea81d0ba1cc4ea3375a3312e385cb4018
aemullin <amullin68@gmail.com>
2022-02-21 10:53:41 -0800

    Make registration page work

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import './registration-view.scss'
+import axios from 'axios';
 
 export function RegistrationView(props) {
     const [ username, setUsername ] = useState ('');
@@ -13,39 +14,55 @@ export function RegistrationView(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username,password, email, birthday);
-        props.onLoggedIn(username);
+        
+        axios.post('https://movie-list-api-5858.herokuapp.com/users', {
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday
+        })
+        .then(response => {
+            const data = response.data;
+            console.log(data);
+            window.open('/', '_self');
+        })
+        .catch(e => {
+            console.log('error with registration')
+        });
     };
 
     return (
-        <Form>
-            <Form.Group controlId="formUsername">  
-                <Form.Label>Username:</Form.Label>
-                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
-                <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-                <Form.Label>Email:</Form.Label>
-                <Form.Control type="text" onChange={e => setEmail(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formBirthday">
-                <Form.Label>Birthday:</Form.Label>
-                <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
-            </Form.Group>
-            <Button type="submit" onClick={handleSubmit}>Submit</button>
-        </Form>
+       <div> 
+           <h1>Sign up for Movie List</h1>
+            <Form>
+                <Form.Group controlId="formUsername">  
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control type="text" onChange={e => setEmail(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formBirthday">
+                    <Form.Label>Birthday:</Form.Label>
+                    <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
+                </Form.Group>
+                <Button type="submit" onClick={handleSubmit}>Submit</Button>
+            </Form>
+        </div>  
     )
 }
 
 RegistrationView.propTypes = {
-    user: PropType.shape({
-        username: PropType.string.isRequired,
-        password: PropType.string.isRequired,
-        email: PropType.string.isRequired,
-        birthday: PropType.string.isRequired
-    }).isRequired,
-    onLoggedIn: PropTypes.func.isRequired
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        birthday: PropTypes.string.isRequired
+    }),
+    onLoggedIn: PropTypes.func
 };
