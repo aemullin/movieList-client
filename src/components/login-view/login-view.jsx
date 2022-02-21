71b7503e13d368b98649971bd2492b4b000eb89f
aemullin <amullin68@gmail.com>
2022-02-21 10:51:34 -0800

    Connect login to API

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 import './login-view.scss'
 
@@ -11,23 +12,36 @@ export function LoginView(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username,password);
-        props.onLoggedIn(username);
+        
+        axios.post('https://movie-list-api-5858.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+        .then(response => {
+            const data = response.data;
+            props.onLoggedIn(data)
+        })
+        .catch(e => {
+                console.log('user does not exist');
+                window.alert('wrong username or password');
+            });
     };
 
     return (
-            <Form>
-                <Form.Group controlId="formUsername">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>Login</Button>
-                <Button variant="secondary">No account? Sign up here!</Button>
-            </Form>
+            <div>
+                <h1>Log in to Movie List</h1>
+                <Form>
+                    <Form.Group controlId="formUsername">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={handleSubmit}>Login</Button>
+                </Form>
+            </div>
     );
 }
 
@@ -35,6 +49,6 @@ LoginView.propTypes = {
     user: PropTypes.shape({
         username: PropTypes.string.isRequired,
         password: PropTypes.string.isRequired
-    }).isRequired,
+    }),
     onLoggedIn: PropTypes.func.isRequired
 };
